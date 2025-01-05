322856aae20074ed6cbdc8849ac4445daf67742a
BOOK-SPF4J9F7VP\leona <24hwww@gmail.com>
2025-01-05 11:43:07 -0300

    app.config.js

--- app.config.js
+++ app.config.js
@@ -1,20 +1,19 @@
-{
-    "expo": {
-      "name": "Faixapp",
-      "slug": "faixapp",
-      "platforms": ["ios", "android"],
-      "version": "1.0.0",
-      "orientation": "portrait",
-      "icon": "./assets/icon.png",
-      "splash": {
-        "image": "./assets/splash.png",
-        "resizeMode": "contain",
-        "backgroundColor": "#ffffff"
-      },
-      "android": {
-        "package": "com.soporte24hwww.faixapp",
-        "versionCode": 1
-      }
+module.exports = {
+  expo: {
+    name: "Faixapp",
+    slug: "faixapp",
+    platforms: ["ios", "android"],
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    android: {
+      package: "com.soporte24hwww.faixapp",
+      versionCode: 1
     }
   }
-  
+};
